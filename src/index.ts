cc9e0da6dc69ad9af88f21266b871fdaeefdea6f
Lucas <lucasnrgaard@gmail.com>
2025-03-14 18:51:56 +0100

    feat: integrate apache-autoindex-parse for directory listing and remove raw route

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,48 @@
 import type { ApiError, HonoEnv } from "./types";
+import { parse } from "apache-autoindex-parse";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
+import { proxy } from "hono/proxy";
 import { cache } from "./cache";
-import { RAW_ROUTER } from "./routes/raw";
 
 const app = new Hono<HonoEnv>();
 
 app.get(
   "*",
   cache({
-    cacheName: "my-app",
+    cacheName: "unicode-proxy",
     cacheControl: "max-age=3600",
   }),
 );
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get("/", async (c) => {
+  const response = await fetch("https://unicode.org/Public?F=0");
+  const html = await response.text();
+  const files = parse(html, "F0");
+  return c.json(files);
 });
 
-app.route("/raw", RAW_ROUTER);
+app.get("/:path", async (c) => {
+  const res = await proxy(`https://unicode.org/Public/${c.req.param("path")}?F=0`);
+
+  if (!res.ok) {
+    return c.json({
+      path: c.req.path,
+      status: res.status,
+      message: res.statusText,
+      timestamp: new Date().toISOString(),
+    } satisfies ApiError);
+  }
+
+  // if the response is a directory, parse the html and return the files
+  if (res.headers.get("content-type")?.includes("text/html")) {
+    const html = await res.text();
+    const files = parse(html, "F0");
+    return c.json(files);
+  }
+
+  return res;
+});
 
 app.onError(async (err, c) => {
   console.error(err);
--- src/routes/raw.ts
+++
@@ -1,9 +0,0 @@
-import type { HonoEnv } from "../types";
-import { Hono } from "hono";
-import { proxy } from "hono/proxy";
-
-export const RAW_ROUTER = new Hono<HonoEnv>();
-
-RAW_ROUTER.get("/:path", async (c) => {
-  return proxy(`https://unicode.org/Public/${c.req.param("path")}`);
-});
