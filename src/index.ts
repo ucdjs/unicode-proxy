477847270399692ec7ae340c3f3c406741cdea6a
Lucas <lucasnrgaard@gmail.com>
2025-03-14 16:46:52 +0100

    chore: fix test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,47 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+interface ApiError {
+  path: string;
+  status: number;
+  message: string;
+  timestamp: string;
+}
+
+app.onError(async (err, c) => {
+  console.error(err);
+  const url = new URL(c.req.url);
+  if (err instanceof HTTPException) {
+    return c.json({
+      path: url.pathname,
+      status: err.status,
+      message: err.message,
+      timestamp: new Date().toISOString(),
+    } satisfies ApiError, err.status);
+  }
+
+  return c.json({
+    path: url.pathname,
+    status: 500,
+    message: "Internal server error",
+    timestamp: new Date().toISOString(),
+  } satisfies ApiError, 500);
+});
+
+app.notFound(async (c) => {
+  const url = new URL(c.req.url);
+  return c.json({
+    path: url.pathname,
+    status: 404,
+    message: "Not found",
+    timestamp: new Date().toISOString(),
+  } satisfies ApiError, 404);
 });
 
 export default app;
